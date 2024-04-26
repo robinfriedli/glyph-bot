fa1806a6bdae843ad40b8fcce129d29dfa6ba9f3
Robin Friedli <robinfriedli@icloud.com>
2024-04-26 18:38:20 +0200

    implement environment variable handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use dotenvy::dotenv;
+
 fn main() {
-    println!("Hello, world!");
+    dotenvy::from_path(
+        std::env::current_dir()
+            .map(|wd| wd.join(".env.local"))
+            .unwrap(),
+    )
+    .ok();
+    dotenvy::from_path(
+        std::env::current_dir()
+            .map(|wd| wd.join(".env.secret"))
+            .unwrap(),
+    )
+    .ok();
+    dotenv().ok();
 }
